b2313c661a7f7726959d2214038716547df47261
icarogoggin <icaro.goggin@gmail.com>
2021-06-24 22:19:06 -0300

    Update codelabs.sql

--- Aula14/codelabs.sql
+++ Aula14/codelabs.sql
@@ -2,3 +2,5 @@
 SELECT nome, sobrenome, email FROM funcionarios
 /*2)Listar o nome, sobrenome e email de todos os funcionários que são representantes de vendas*/
 SELECT nome, sobrenome, email FROM funcionarios WHERE cargo = 'Sales Rep'
+/*3)Listar todos os cargos (sem repetir)*/
+SELECT DISTINCT (cargo) FROM funcionarios
